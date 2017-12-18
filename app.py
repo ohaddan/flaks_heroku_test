11b3cbe9f82cb4f349f43b89f0cf51716d902994
ohaddan <ohad.dan@gmail.com>
2017-12-18 18:20:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-# # ./app.py
-# from flask import Flask
-# app = Flask(__name__)
-# @app.route('/')
-# def index():
-# 	return 'Yo, its working!'
-# if __name__ == "__main__":
-# 	app.run()
-
 from flask import Flask
-
 app = Flask(__name__)
-app.run()
 
-from app import views
+
+import views
