643fef9efd6c99e055e7edc54e9e4aa8283c9e92
ohaddan <ohad.dan@gmail.com>
2017-12-19 09:00:04 +0200

    Add files via upload

---
+++ project_versions/3_tempaltes/views.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'nickname': 'Ted'}
+    return render_template('index.html',
+                           title='Home',
+                           user=user)
+
+
+    
